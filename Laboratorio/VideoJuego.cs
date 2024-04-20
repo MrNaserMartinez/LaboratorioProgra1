8b226888b250057c33b3119d9edce87860d018a8
MrNaser <nmartinezm10@miumg.edu.gt>
2024-04-20 12:47:44 -0600

    cabal

--- Laboratorio/VideoJuego.cs
+++ Laboratorio/VideoJuego.cs
@@ -1,103 +1,174 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LaboratorioProgra1.Laboratorio
+﻿internal class VideoJuego
 {
-    /// <summary>
-    /// Esta Clase Padre tiene 4 errores lógicos
-    /// debe de encontrarlos y corregirlos
-    /// documente los errores encontrados y las correcciones realizadas
-    /// </summary>
-    internal class VideoJuego
+    public string titulo;
+    public string desarrolladora;
+    public double calificacion;
+    public int anioLanzamiento;
+    public bool jugando;
+
+    public string Titulo
     {
-        public string titulo;
-        public string desarrolladora;
-        public double calificacion;
-        public int anioLanzamiento;
-        public bool jugando;
+        get { return titulo; }
+        set { titulo = value; }
+    }
 
-        public string Titulo
-        {
-            get { return titulo; }
-            set { titulo = value; }
-        }
+    public string Desarrolladora
+    {
+        get { return desarrolladora; }
+        set { desarrolladora = value; }
+    }
 
-        public string Desarrolladora
+    public double Calificacion
+    {
+        get { return calificacion; }
+        set
         {
-            get { return desarrolladora; }
-            set { desarrolladora = value; }
-        }
-
-        public double Calificacion
-        {
-            get { return calificacion; }
-            set
+            // La calificación debe estar entre 0 y 10
+            if (value >= 0 && value <= 10)
             {
-                //  La calificación debe estar entre 0 y 10
-                if (value < 0 || value > 10)
-                {
-                    calificacion = value;
-                } else
-                {
-                    Console.WriteLine("La calificación debe estar entre 0 y 10");
-                }
-                    
-            }
-        }
-
-        public int AnioLanzamiento
-        {
-            get { return anioLanzamiento; }
-            set
-            {
-                // Error lógico: El año de lanzamiento 
-                anioLanzamiento = value;
-            }
-        }
-
-        public void MostrarInformacion()
-        {
-            Console.WriteLine("Título: " + Titulo);
-            Console.WriteLine("Desarrolladora: " + Desarrolladora);
-            Console.WriteLine("Calificación: " + Calificacion);
-            Console.WriteLine("Año de Lanzamiento: " + AnioLanzamiento);
-        }
-
-        public static void saludo()
-        {
-            Console.WriteLine("Hi Gamer:");
-        }
-
-
-        public void IniciarPartida()
-        {
-            jugando = true;
-            Console.WriteLine("¡Partida iniciada!");
-        }
-
-        public void DetenerPartida()
-        {
-            // Error lógico
-            Console.WriteLine("Partida detenida.");
-
-        }
-
-        public void TerminarJuego()
-        {
-            // Error lógico
-            if (jugando)
-            {
-                
-                Console.WriteLine("Juego terminado.");
+                calificacion = value;
             }
             else
             {
-                Console.WriteLine("No hay partida en curso.");
+                Console.WriteLine("La calificación debe estar entre 0 y 10");
             }
         }
+    }
 
+    public int AnioLanzamiento
+    {
+        get { return anioLanzamiento; }
+        set
+        {
+            // El año de lanzamiento debe ser mayor o igual a 1950 y menor o igual al año actual
+            if (value >= 1950 && value <= 2024)
+            {
+                anioLanzamiento = value;
+            }
+            else
+            {
+                Console.WriteLine("El año de lanzamiento debe ser mayor o igual a 1950 y menor o igual al año actual.");
+            }
+        }
+    }
+
+    // Constructores
+    public VideoJuego() { }
+
+    public VideoJuego(string titulo, string desarrolladora, double calificacion, int anioLanzamiento)
+    {
+        Titulo = titulo;
+        Desarrolladora = desarrolladora;
+        Calificacion = calificacion;
+        AnioLanzamiento = anioLanzamiento;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Título: " + Titulo);
+        Console.WriteLine("Desarrolladora: " + Desarrolladora);
+        Console.WriteLine("Calificación: " + Calificacion);
+        Console.WriteLine("Año de Lanzamiento: " + AnioLanzamiento);
+    }
+
+    public static void Saludo()
+    {
+        Console.WriteLine("Hi Gamer:");
+    }
+
+    public void IniciarPartida()
+    {
+        jugando = true;
+        Console.WriteLine("¡Partida iniciada!");
+    }
+
+    public void DetenerPartida()
+    {
+        jugando = false; // Establecer jugando a false
+        Console.WriteLine("Partida detenida.");
+    }
+
+    public void TerminarJuego()
+    {
+        if (!jugando) // Cambiar la condición
+        {
+            Console.WriteLine("No hay partida en curso.");
+        }
+        else
+        {
+            Console.WriteLine("Juego terminado.");
+            jugando = false; // Establecer jugando a false
+        }
+    }
+}
+
+// Clase estática GestorDeVideojuegos
+internal static class GestorDeVideojuegos
+{
+    private static List<VideoJuego> listaDeJuegos = new List<VideoJuego>();
+
+    public static List<VideoJuego> ObtenerListaDeJuegos()
+    {
+        return listaDeJuegos;
+    }
+
+    public static void AgregarJuego(VideoJuego juego)
+    {
+        listaDeJuegos.Add(juego);
+    }
+}
+
+// Clases derivadas de VideoJuego
+internal class JuegoAccion : VideoJuego
+{
+    public int NivelDificultad { get; set; }
+
+    public JuegoAccion(string titulo, string desarrolladora, double calificacion, int anioLanzamiento, int nivelDificultad)
+        : base(titulo, desarrolladora, calificacion, anioLanzamiento)
+    {
+        NivelDificultad = nivelDificultad;
+    }
+
+    public void MostrarMensajeDificultad()
+    {
+        Console.WriteLine($"Nivel de dificultad: {NivelDificultad}");
+    }
+
+    // Sobrecarga de operadores
+    public static JuegoAccion operator +(JuegoAccion juego, int incremento)
+    {
+        JuegoAccion nuevoJuego = new JuegoAccion(juego.Titulo, juego.Desarrolladora, juego.Calificacion, juego.AnioLanzamiento, juego.NivelDificultad + incremento);
+        return nuevoJuego;
+    }
+}
+internal class JuegoAventura : VideoJuego
+{
+    public int horasEstimadas;
+
+    public JuegoAventura(string titulo, string desarrolladora, double calificacion, int anioLanzamiento, int horasEstimadas)
+        : base(titulo, desarrolladora, calificacion, anioLanzamiento)
+    {
+        this.horasEstimadas = horasEstimadas;
+    }
+
+    public void MostrarDuracion()
+    {
+        Console.WriteLine($"Duración estimada: {horasEstimadas} horas");
+    }
+}
+
+internal class JuegoDeportes : VideoJuego
+{
+    public string deporte;
+
+    public JuegoDeportes(string titulo, string desarrolladora, double calificacion, int anioLanzamiento, string deporte)
+        : base(titulo, desarrolladora, calificacion, anioLanzamiento)
+    {
+        this.deporte = deporte;
+    }
+
+    public void MostrarDeporte()
+    {
+        Console.WriteLine($"Deporte: {deporte}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,31 +1,59 @@
-﻿using LaboratorioProgra1.EjemploPolimorfismo;
-using LaboratorioProgra1.EjemploPolimorfismo.Heredados;
-using LaboratorioProgra1.Laboratorio;
-
-static void EjemploPolimorfismo()
+﻿class Program
 {
-    Animal[] animales = new Animal[3];
-    animales[0] = new Tortuga();
-    animales[1] = new Gato();
-    animales[2] = new Perro();
-    foreach (Animal animal in animales)
+    static void Main(string[] args)
     {
-        animal.HacerSonido();
+        VideoJuego.Saludo();
+
+        // Crear instancia de VideoJuego para The Legend of Zelda: Breath of the Wild
+        VideoJuego zelda = new VideoJuego
+        {
+            Titulo = "The Legend of Zelda: Breath of the Wild",
+            Desarrolladora = "Nintendo",
+            Calificacion = 10,
+            AnioLanzamiento = 2017
+        };
+
+        // Agregar el juego Zelda a la lista de juegos
+        GestorDeVideojuegos.AgregarJuego(zelda);
+
+        // Crear instancias de clases derivadas
+        JuegoAccion juegoAccion = new JuegoAccion("Gears of War", "Epic Games", 9.5, 2006, 5);
+        JuegoAventura juegoAventura = new JuegoAventura("Final Fantasy 7 Remake", "Square Enix", 10, 2015, 70);
+        JuegoDeportes juegoDeportes = new JuegoDeportes("FIFA 22", "EA Sports", 8.7, 2021, "Fútbol");
+
+        // Agregar juegos a la lista
+        GestorDeVideojuegos.AgregarJuego(juegoAccion);
+        GestorDeVideojuegos.AgregarJuego(juegoAventura);
+        GestorDeVideojuegos.AgregarJuego(juegoDeportes);
+
+        // Mostrar información de los juegos
+        foreach (VideoJuego juego in GestorDeVideojuegos.ObtenerListaDeJuegos())
+        {
+            juego.MostrarInformacion();
+
+            // Llamar al método específico de la clase derivada (polimorfismo)
+            if (juego is JuegoAccion juegoAccion1)
+            {
+                juegoAccion1.MostrarMensajeDificultad();
+            }
+            else if (juego is JuegoAventura juegoAventura1)
+            {
+                juegoAventura1.MostrarDuracion();
+            }
+            else if (juego is JuegoDeportes juegoDeportes1)
+            {
+                juegoDeportes1.MostrarDeporte();
+            }
+
+            Console.WriteLine();
+        }
+
+        // Ejemplo de sobrecarga de operadores
+        JuegoAccion juegoAccion2 = new JuegoAccion("Doom Eternal", "id Software", 8.9, 2020, 4);
+        JuegoAccion juegoAccion3 = juegoAccion2 + 1; // Sobrecarga del operador +
+        juegoAccion3.MostrarMensajeDificultad();
+
+        Console.ReadLine();
     }
-}   
-
-static void Laboratorio()
-{
-    VideoJuego videoJuego = new VideoJuego();
-    videoJuego.Titulo = "The Legend of Zelda: Breath of the Wild";
-    videoJuego.Desarrolladora = "Nintendo";
-    videoJuego.Calificacion = 10; //la calificacion es un 10 solo por ser zelda :), el error es que lo estaba asignando en 50
-    videoJuego.anioLanzamiento = 2017; //en program tenia el error que AnioLanzamiento estaba en mayuscula cuando es anioLandamiento
-    videoJuego.MostrarInformacion();
 }
-
-//Laboratorio();
-VideoJuego.listaDeJuegos();
-
-
 
